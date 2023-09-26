71de24353f6bc97d43a31e3c1f147b6ebe3c3d9a
MUHAMMEDFARHAN007 <146074638+MUHAMMEDFARHAN007@users.noreply.github.com>
2023-09-26 15:59:11 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,38 @@
+package javalab;
+import java.util.Scanner;
+public class marks {
+public static void main(String [] args)
+{
+	int marks,grade;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the marks:");
+	marks=sc.nextInt();
+	if(marks<=100&&marks>90) {
+		grade=10;
+		System.out.println("Grade="+grade);
+	}
+	else if(marks<90&&marks>80)
+	{
+		grade=8;
+		System.out.println("Grade="+grade);
+	}
+	else if(marks<80&&marks>70)
+	{
+		grade=7;
+		System.out.println("Grade="+grade);
+	}
+	else if(marks<70&&marks>60)
+	{
+		grade=6;
+		System.out.println("Grade="+grade);
+	}
+	else if(marks<60&&marks>50)
+	{
+		grade=5;
+		System.out.println("Grade="+grade);
+	}
+	else 
+		System.out.println("fail");
+	
+}
+}
