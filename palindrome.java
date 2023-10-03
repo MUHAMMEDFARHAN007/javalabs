a24469bd45e67a1192e84ee59172a01966fd833b
MUHAMMEDFARHAN007 <146074638+MUHAMMEDFARHAN007@users.noreply.github.com>
2023-10-03 15:47:32 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,26 @@
+package javalab;
+import java.util.Scanner;
+public class palindrome {
+public static void main(String [] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter a string:");
+	String name= sc.next();
+	boolean ispal = check(name);
+	if(ispal==true)
+		System.out.println("string is palindrome");
+	else
+		System.out.println("string is not palindrome");
+}
+static boolean check(String name)
+{
+	char [] chararray = name.toCharArray();
+	int length = name.length();
+	for(int i=0;i<length/2;i++)
+	{
+		if(chararray[i]!=chararray[length-1-i])
+			return false;
+	}
+	return true;
+}
+}
